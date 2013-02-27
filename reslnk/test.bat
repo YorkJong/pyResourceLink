d8559856f8a1d181b7c5be0d58616c7ab4aa1ecb
York <yukuan.jiang@gmail.com>
2013-02-28 01:07:29 +0800

    ResLnk: Fine tuned test.bat and demo.bat

--- reslnk/test.bat
+++ reslnk/test.bat
@@ -4,18 +4,18 @@ set reslnk=reslnk.py
 set src=res.lst
 set tgt=res.bin
 set dir=res
-echo =^> Link resource files into single one.
+echo =^> Link resource files into single one (%tgt%).
 %reslnk% link -d%dir% -o%tgt% %src%
 
 set src=res.lst
 set tgt=res_map.i
 set dir=res
-echo =^> Generate a resource map file in format of C array..
+echo =^> Generate a resource map file (%tgt%) in format of C array.
 %reslnk% map -d%dir% -o%tgt% %src%
 
 set src=res.lst
 set tgt=ResID.h
-echo =^> Generate a C header file of resource ID enumeration.
+echo =^> Generate a C header file (%tgt%) of resource ID enumeration.
 %reslnk% id -o%tgt% %src%
 
 pause

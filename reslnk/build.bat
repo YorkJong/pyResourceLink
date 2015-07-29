8ca1ee7b2184e239b71e975c880c13510ad4057e
York <yukuan.jiang@gmail.com>
2015-07-29 18:54:50 +0800

    build.bat: Updated files for pyinstaller  v2.1

--- reslnk/build.bat
+++ reslnk/build.bat
@@ -1,15 +1,13 @@
 del %1.exe
-set base=%Python_home%\Tools\pyinstaller-1.5.1
-%base%\Makespec.py --onefile --upx %1.py
-%base%\Build.py %1.spec
+pyinstaller --onefile %1.py
 
-move .\dist\%1.exe .
+move dist\%1.exe .
 
-@echo off
 del /Q %1.spec
-del /Q warn%1.txt
-rem del *.pyc
+rem del /Q *.spec
 
 rd /Q /S build
 rd /Q /S dist
-del /Q logdict*.log
+
+del *.pyc
+
